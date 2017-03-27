5aab48e2e8c54951f46f4827b92fbaed139b9a89
Julieta Salvadó <julieta.salvado@gmail.com>
2017-03-27 01:12:22 -0300

    Add Link Components

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -5,19 +5,22 @@
 "use strict";
 
 var React = require('react');
+var Route = require('react-router');
+var Link = Route.Link;
 
+//Links go to names in routes.js
 var Header = React.createClass({
     render: function () {
         return (
             <nav className="navbar navbar-default">
                 <div className="container-fluid">
-                    <a href="/" className="navbar-brand">
+                    <Link to="app" className="navbar-brand">
                         <img src="images/Logo.jpg" />
-                    </a>
+                    </Link>
                     <ul className="nav navbar-nav">
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/#authors">Authors</a> </li>
-                        <li><a href="/#about">About</a></li>
+                        <li><Link to="app">Home</Link></li>
+                        <li><Link to="authors">Authors</Link> </li>
+                        <li><Link to="about">About</Link></li>
                     </ul>
                 </div>
             </nav>
