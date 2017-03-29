b3cedc76d2542ae8e5a7273d60b337eb753df7d9
Julieta Salvadó <julieta.salvado@gmail.com>
2017-03-28 22:59:04 -0300

    Add transitions in About page

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -7,6 +7,24 @@
 var React = require('react');
 
 var About = React.createClass({
+    statics: {
+        //when this page can be transition to?
+        willTransitionTo: function (transition, params, query, callback) {
+            if (!confirm('Are you sure you want to read a page that\'s this boring?')) {
+                transition.about(); //abort
+            } else {
+                callback(); //go on
+            }
+        },
+
+        //if the user wants to leave
+        willTransitionFrom: function (transition, component) {
+            if (!confirm('Are you sure you want to leave a page that\'s this exciting?')) {
+                transition.about();//abort
+            }
+        }
+    },
+
     render: function () {
         return (
             <div>
